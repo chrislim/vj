f6b5860dc8d540b6899495230e125480ed761c01
Chris <kirisu@gmail.com>
2019-10-29 12:17:48 -0700

    Shippable result

--- index.js
+++ index.js
@@ -7,7 +7,28 @@ const now = moment();
 const results = [];
 for (let i = 0; i < 7; i++) {
   now.day(i);
-  results.push(vj(now.day(), now.date()));
+  results.push([now.format('dddd, MMMM Do'), vj(now.day(), now.date())]);
+}
+
+function timeslotsToText (slots) {
+  let strs = slots.map(s => {
+    return `${s.start}-${s.end}`;
+  });
+  return strs.join('<br/>');
 }
 
 console.log(today, results);
+const elem = document.getElementById('visitation');
+results.forEach(r => {
+  const row = document.createElement('div');
+  let str = `<h2>${r[0]}</h2><p>`;
+  if (r[1].detentionCenterIsOpen) {
+    str += timeslotsToText(r[1].timeslots);
+  } else {
+    str += ' Closed';
+  }
+  str += '</p>';
+  row.innerHTML = str;
+  elem.appendChild(row);
+});
+
--- vj.js
+++ vj.js
@@ -1,4 +1,4 @@
-(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 const vj = require('./visitJojo');
 const moment = require('moment');
 
@@ -8,10 +8,31 @@ const now = moment();
 const results = [];
 for (let i = 0; i < 7; i++) {
   now.day(i);
-  results.push(vj(now.day(), now.date()));
+  results.push([now.format('dddd, MMMM Do'), vj(now.day(), now.date())]);
+}
+
+function timeslotsToText (slots) {
+  let strs = slots.map(s => {
+    return `${s.start}-${s.end}`;
+  });
+  return strs.join('<br/>');
 }
 
 console.log(today, results);
+const elem = document.getElementById('visitation');
+results.forEach(r => {
+  const row = document.createElement('div');
+  let str = `<h2>${r[0]}</h2><p>`;
+  if (r[1].detentionCenterIsOpen) {
+    str += timeslotsToText(r[1].timeslots);
+  } else {
+    str += ' Closed';
+  }
+  str += '</p>';
+  row.innerHTML = str;
+  elem.appendChild(row);
+});
+
 
 },{"./visitJojo":3,"moment":2}],2:[function(require,module,exports){
 //! moment.js
